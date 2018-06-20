be5ee95c79d54f61845a2851d18774662375684b
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-06-20 11:01:48 -0500

    intiial commit

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I made a change...</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
