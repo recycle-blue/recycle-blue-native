4ac8f0bfcbaed910edd5e319bf6daa6b84830db2
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-06-21 11:56:36 -0500

    got a map to display

---
+++ client/components/MapComp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { MapView, Marker } from 'expo'
+
+const MapComp = () => {
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  )
+}
+
+export default MapComp
--- client/navigation/Navigation.js
+++ client/navigation/Navigation.js
@@ -2,21 +2,27 @@ import React from 'react'
 import { Button } from 'react-native'
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation'
 import { Home, AddProduct } from '../components'
+import MapComp from '../components/MapComp'
 
-const Drawer = createDrawerNavigator({
-  home: { screen: Home },
-  addProduct: { screen: AddProduct }
-}, {
+const Drawer = createDrawerNavigator(
+  {
+    home: { screen: Home },
+    addProduct: { screen: AddProduct },
+    map: { screen: MapComp },
+  },
+  {
     drawerOpenRoute: 'DrawerOpen',
     drawerCloseRoute: 'DrawerClose',
-    drawerToggleRoute: 'DrawerToggle'
+    drawerToggleRoute: 'DrawerToggle',
   }
 )
 
-const PrimaryNav = createStackNavigator({
-  home: { screen: Home },
-  drawerStack: { screen: Drawer }
-}, {
+const PrimaryNav = createStackNavigator(
+  {
+    home: { screen: Home },
+    drawerStack: { screen: Drawer },
+  },
+  {
     title: 'main',
     initialRouteName: 'drawerStack',
     headerMode: 'float',
@@ -24,8 +30,10 @@ const PrimaryNav = createStackNavigator({
       headerStyle: { backgroundColor: 'blue' },
       title: 'RecycleBlue',
       headerTintColor: 'white',
-      headerLeft: <Button title='pressme' onPress={() => navigation.toggleDrawer()} />
-    })
+      headerLeft: (
+        <Button title="pressme" onPress={() => navigation.toggleDrawer()} />
+      ),
+    }),
   }
 )
 
