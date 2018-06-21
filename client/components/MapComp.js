cf28e02ca4907692f6694f341ccfc7f642d6e9d8
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-06-21 15:47:03 -0500

    location markers work

--- client/components/MapComp.js
+++ client/components/MapComp.js
@@ -1,18 +1,45 @@
 import React from 'react'
-import { MapView, Marker } from 'expo'
+import { MapView } from 'expo'
+import axios from 'axios'
+import { googleAPIKey } from '../secrets'
 
-const MapComp = () => {
-  return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  )
+class MapComp extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      markers: [],
+    }
+  }
+  async componentDidMount() {
+    const { data } = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.895766,-87.638865&keyword=recycle&radius=3000&key=${googleAPIKey}`
+    )
+    console.log('RESULTS:', data.results[0].geometry.location)
+    this.setState({ markers: data.results })
+  }
+  render() {
+    const { markers } = this.state
+    return (
+      <MapView
+        provider="google"
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 41.895766,
+          longitude: -87.638865,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {markers.map(marker => {
+          const location = {
+            latitude: marker.geometry.location.lat,
+            longitude: marker.geometry.location.lng,
+          }
+          return <MapView.Marker key={marker.id} coordinate={location} />
+        })}
+      </MapView>
+    )
+  }
 }
 
 export default MapComp
