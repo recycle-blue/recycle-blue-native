d9d0230a9e62b9ce941b941253971d0e5cd5db26
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-06-20 21:24:44 -0500

    add product model tests

--- back-end/server/db/models/product.spec.js
+++ back-end/server/db/models/product.spec.js
@@ -8,13 +8,36 @@ describe('Product model', () => {
     points: 10,
     recycleUse: 'Put it in the recycling bin'
   }
-  it('creates a product with the proper fields', () => {
-    return Product.create(plasticBottle).then(newProduct => {
-      expect(newProduct).to.be.an('object')
-      expect(newProduct.name).to.equal('cool bottle')
-      expect(newProduct.description).to.equal('Do not let whales eat this')
-      expect(newProduct.points).to.equal(10)
-      expect(newProduct.recycleUse).to.equal('Put it in the recycling bin')
-    })
+  let nullName = {
+    description: 'something',
+    points: 11,
+    recycleUse: "I don't know"
+  }
+  let emptyName = {
+    name: '',
+    points: 11,
+    recycleUse: 'Whatever'
+  }
+  it('creates a product with the proper fields', async () => {
+    const newProduct = await Product.create(plasticBottle)
+    expect(newProduct).to.be.an('object')
+    expect(newProduct.name).to.equal('cool bottle')
+    expect(newProduct.description).to.equal('Do not let whales eat this')
+    expect(newProduct.points).to.equal(10)
+    expect(newProduct.recycleUse).to.equal('Put it in the recycling bin')
+  })
+  it('properly validates null', async () => {
+    try {
+      await Product.create(nullName)
+    } catch (err) {
+      expect(err.message).to.contain('notNull')
+    }
+  })
+  it('properly validates empty', async () => {
+    try {
+      await Product.create(emptyName)
+    } catch (err) {
+      expect(err.message).to.contain('Validation')
+    }
   })
 })
