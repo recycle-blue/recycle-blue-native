c886fd37f05098b96200bc42ead3f22478efb780
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-06-21 15:49:12 -0500

    updating redux store with location

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
+import location from './location'
 
-const reducer = combineReducers({ user })
+const reducer = combineReducers({ user, location })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
