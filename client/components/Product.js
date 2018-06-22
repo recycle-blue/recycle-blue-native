2b95fbdbf3870fa540ff6956c4a1a14f519d38a9
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-06-22 13:39:25 -0500

    product page done awaiting activity data

--- client/components/Product.js
+++ client/components/Product.js
@@ -10,15 +10,40 @@ class Product extends React.Component {
   }
 
   componentWillMount() {
-    getProduct(this.props.productId)
+    this.props.getProduct(this.props.productId)
   }
 
   render() {
     return (
-      <Text>This is working</Text>
+      <Container>
+        <Content>
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <Text>This is where the image goes</Text>
+            <Image
+              style={styles.image}
+              source={{ uri: this.props.photo }}
+            />
+            <Text>{this.props.points}</Text>
+            <Text>{this.props.description}</Text>
+            <Text>{this.props.recycleUse}</Text>
+          </View>
+        </Content>
+      </Container>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    width: 250,
+    height: 250,
+    borderWidth: 1,
+    borderColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 const mapStateToProps = state => {
   return ({
