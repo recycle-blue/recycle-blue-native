422cd0e8b5b996cac6a6545444fcd219586c137c
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-06-22 15:41:50 -0500

    clean

--- back-end/server/api/product.js
+++ back-end/server/api/product.js
@@ -3,19 +3,19 @@ const { Product } = require('../db/models')
 module.exports = router
 
 
-router.get('/', async (req, res, next) => {
+router.get('/:productId', async (req, res, next) => {
   try {
-    const products = await Product.findAll()
-    res.json(products)
+    const product = await Product.findById(req.params.productId)
+    res.json(product)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:productId', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const product = await Product.findById(req.params.productId)
-    res.json(product)
+    const products = await Product.findAll()
+    res.json(products)
   } catch (err) {
     next(err)
   }
--- back-end/server/api/users.js
+++ back-end/server/api/users.js
@@ -2,14 +2,14 @@ const router = require('express').Router()
 const { User } = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll()
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const users = await User.findAll()
+//     res.json(users)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 router.get('/:userId', async (req, res, next) => {
   try {
