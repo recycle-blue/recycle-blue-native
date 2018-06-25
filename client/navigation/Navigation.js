a5d5ea7ba737272f8e9f3d22f9a6bdc63c9a1707
AndrewTrahan <globalART19@gmail.com>
2018-06-25 17:25:36 -0500

    renamed product page

--- client/navigation/Navigation.js
+++ client/navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from 'react-native'
 import { createStackNavigator, createDrawerNavigator, createSwitchNavigator } from 'react-navigation'
-import { Login, AddActivity, Camera, MapComp, Dashboard, Product, LoadingScreen, Leaderboard, Friends } from '../components'
+import { Login, AddActivity, Camera, MapComp, Dashboard, Activity, LoadingScreen, Leaderboard, Friends } from '../components'
 
 const Drawer = createDrawerNavigator(
   {
@@ -9,10 +9,10 @@ const Drawer = createDrawerNavigator(
     addActivity: { screen: AddActivity },
     map: { screen: MapComp },
     camera: { screen: Camera },
-    product: { screen: Product },
+    activity: { screen: Activity },
     loadingScreen: { screen: LoadingScreen },
-    Leaderboard: { screen: Leaderboard},
-    Friends: { screen: Friends}
+    Leaderboard: { screen: Leaderboard },
+    Friends: { screen: Friends }
   },
   {
     drawerOpenRoute: 'DrawerOpen',
