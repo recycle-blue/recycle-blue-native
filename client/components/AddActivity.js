78bc48975365f14ab0349134f8a950a3e1a7736d
Lams <lsadoun@mail.bradley.edu>
2018-06-26 21:52:20 -0500

    Update AddActivity.js

--- client/components/AddActivity.js
+++ client/components/AddActivity.js
@@ -107,7 +107,6 @@ class AddActivity extends React.Component {
 }
 
 const mapStateToProps = (store) => {
-  console.log('store', store)
   return {
     userId: store.user.id,// || 1,
     name: store.product.name,// || "bottle",
