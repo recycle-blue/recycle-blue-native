0e5acb4db305e343b24bc194ba0c42b76651c385
AndrewTrahan <globalART19@gmail.com>
2018-06-27 18:15:16 -0500

    started bug fix here...oops

--- back-end/server/api/activity.js
+++ back-end/server/api/activity.js
@@ -35,13 +35,12 @@ router.get("/weekly/:userId", async (req, res, next) => {
 
 router.get('/:activityId/ad', async (req, res, next) => {
   try {
-    const adData = await Ad.find({
+    const ad = await Ad.find({
       where: {
         activityId: +req.params.activityId,
       },
     })
-    console.log(adData)
-    const ad = adData.dataValues
+    // const ad = adData.dataValues
     res.json(ad)
   } catch (err) {
     next(err)
