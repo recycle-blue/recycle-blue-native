18f1d6dd6a6bfd95392abb5b90dcd7b06ac99eaa
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-06-30 16:37:14 -0500

    styling is complete

--- client/components/ActivityChart.js
+++ client/components/ActivityChart.js
@@ -1,39 +1,50 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import { connect } from 'react-redux'
 import { VictoryChart, VictoryTheme, VictoryArea } from "victory-native";
 import Svg, { Text } from 'react-native-svg';
 
 const data = [
-  { day: "Mon", recycled: 13000 },
-  { day: "Tues", recycled: 16500 },
-  { day: "Wed", recycled: 14250 },
-  { day: "Thurs", recycled: 19000 },
-  { day: "Fri", recycled: 19000 },
-  { day: "Sat", recycled: 19000 },
-  { day: "Sun", recycled: 19000 }
+  { day: "Mon", points: 2 },
+  { day: "Tues", points: 4 },
+  { day: "Wed", points: 6 },
+  { day: "Thurs", points: 0 },
+  { day: "Fri", points: 1 },
+  { day: "Sat", points: 4 },
+  { day: "Sun", points: 9 }
 ];
 
 class ActivityChart extends React.Component {
   render() {
-
+    const { height, width } = Dimensions.get('screen');
+    const halfheight = height / 2
     return (
       <View style={styles.container}>
-        <VictoryChart
-          width={350}
-          theme={VictoryTheme.material}
-          animate={{
-            duration: 1000,
-            onLoad: { duration: 700 }
-          }}>
-          <VictoryArea
-            data={data}
-            x="day"
-            y="recycled"
-            style={{
-              data: { fill: "#c43a31" }
-            }} />
-        </VictoryChart>
+        <Svg height={halfheight} width={width}>
+          <VictoryChart
+            width={350}
+            theme={VictoryTheme.material}
+            animate={{
+              duration: 1000,
+              onLoad: { duration: 700 }
+            }}>
+            <VictoryArea
+              data={data}
+              x="day"
+              y="points"
+              style={{
+                data: { fill: "#3880f4" }
+              }} />
+          </VictoryChart>
+          <Text
+            fill="#3880f4"
+            fontSize="20"
+            fontWeight="bold"
+            x={`${width / 2}`}
+            y={`${(halfheight / 8)}`}
+            textAnchor="middle"
+          >Weekly Activity</Text>
+        </Svg>
       </View>
     );
   }
