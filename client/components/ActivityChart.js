8c3978ba9851485ac5d0a177724838d7e3b0d2e5
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-06-30 13:37:56 -0500

    eh

--- client/components/ActivityChart.js
+++ client/components/ActivityChart.js
@@ -1,61 +1,37 @@
-import React from 'react'
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { connect } from 'react-redux'
-import { Grid, LineChart, XAxis, YAxis } from 'react-native-svg-charts'
-import { View, StyleSheet } from 'react-native'
-import { setActivityWeekThunk } from '../store/activity'
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryArea } from "victory-native";
+
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+];
 
 class ActivityChart extends React.Component {
-  componentWillMount() {
-    this.props.setActivityWeekThunk(this.props.user.id)
-  }
   render() {
-    const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
-
-    const axesSvg = { fontSize: 10, fill: 'grey' };
-    const verticalContentInset = { top: 10, bottom: 10 }
-    const xAxisHeight = 30
-
-    const styles = StyleSheet.create({
-      image: {
-        flex: 1,
-        width: 250,
-        height: 250,
-        borderWidth: 1,
-        borderColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    })
     return (
-      <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
-        <YAxis
-          data={data}
-          style={{ marginBottom: xAxisHeight }}
-          contentInset={verticalContentInset}
-          svg={axesSvg}
-        />
-        <View style={{ flex: 1, marginLeft: 10 }}>
-          <LineChart
-            style={{ flex: 1 }}
-            data={data}
-            contentInset={verticalContentInset}
-            svg={{ stroke: 'rgb(134, 65, 244)' }}
-          >
-            <Grid />
-          </LineChart>
-          <XAxis
-            style={{ marginHorizontal: -10, height: xAxisHeight }}
-            data={data}
-            formatLabel={(value, index) => index}
-            contentInset={{ left: 10, right: 10 }}
-            svg={axesSvg}
-          />
-        </View>
+      <View style={styles.container}>
+        <VictoryChart width={300} theme={VictoryTheme.material}>
+          <VictoryArea data={data} x="quarter" y="earnings" />
+        </VictoryChart>
       </View>
-    )
+    );
   }
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff"
+  }
+});
+
+
 
 const mapStateToProps = (state) => {
   return ({
