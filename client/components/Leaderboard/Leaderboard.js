2d9650fc262ee9661b0fcaf490d6122b206fd05b
AndrewTrahan <globalART19@gmail.com>
2018-07-03 11:19:10 -0500

    leaderboard styling complete

--- client/components/Leaderboard/Leaderboard.js
+++ client/components/Leaderboard/Leaderboard.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 import { connect } from 'react-redux'
-import { Container, Card, CardItem, Title, Header, Left, Right, Icon, Thumbnail } from 'native-base'
+import { Container, Card, CardItem, Thumbnail } from 'native-base'
 import { getLeadersThunk, getUserThunk } from '../../store'
-import { colors } from '../color-palette'
+import { colors, StatusBarHeight } from '../color-palette'
 
 class Leaderboard extends React.Component {
-
   componentDidMount() {
     this.props.getLeaderboard(this.props.user.id)
   }
@@ -19,34 +18,48 @@ class Leaderboard extends React.Component {
   render() {
     const { leaders } = this.props
     return (
-      <Container>
-        <Header>
-          <Title style={{ color: 'blue', fontSize: 30 }}>
+      <Container style={{ backgroundColor: colors.light }} >
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
             Leaderboard
-          </Title>
-        </Header>
+          </Text>
+        </View>
+        <Card style={[styles.card, { maxHeight: 40 }]} >
+          <CardItem style={styles.cardItem}          >
+            <Text style={styles.idxTop}>#</Text>
+            <Text style={styles.numTop} />
+            <Text style={styles.nameTop}>User</Text>
+            <Text style={styles.numTop}>Pts</Text>
+            <Text style={styles.imageTop}>Rank</Text>
+          </CardItem>
+        </Card>
         <ScrollView>
           {leaders.length ?
             leaders.map((leader, idx) =>
-              <Card key={leader.id} >
-                <CardItem button onPress={() => this.handlePress(leader.id)}>
-                  <Left>
-                    <Text>{idx + 1}</Text>
-                  </Left>
-                  <Left>
-                    <Text>{leader.name}</Text>
-                  </Left>
-                  <Right>
-                    {leader.milestone && (
-                      <Thumbnail
-                        name="userMilestoneThumbnail"
-                        small
-                        square
-                        source={{ uri: leader.milestone.badgeIcon }}
-                      />
-                    )}
-                    <Text>{leader.totalPoints}</Text>
-                  </Right>
+              <Card key={leader.id} style={styles.card} >
+                <CardItem
+                  button
+                  onPress={() => this.handlePress(leader.id)}
+                  style={styles.cardItem}
+                >
+                  <Text style={styles.idx}>{idx + 1}</Text>
+                  <Thumbnail
+                    name="userPicThumbnail"
+                    small
+                    source={{ uri: leader.imageUrl }}
+                    style={styles.image}
+                  />
+                  <Text style={styles.name}>{leader.name}</Text>
+                  <Text style={styles.num}>{leader.totalPoints}</Text>
+                  {leader.milestone && (
+                    <Thumbnail
+                      name="userMilestoneThumbnail"
+                      small
+                      square
+                      source={{ uri: leader.milestone.badgeIcon }}
+                      style={styles.image}
+                    />
+                  )}
                 </CardItem>
               </Card>
             )
@@ -64,10 +77,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    marginBottom: 3,
+    marginTop: 2,
+  },
+  cardItem: {
+    flexDirection: 'row',
+  },
+  header: {
+    backgroundColor: colors.midLight,
+    height: 30,
+    width: '80%',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    alignSelf: 'center',
+    marginBottom: 5,
+  },
+  headerText: {
+    color: colors.white,
+    fontSize: 20,
+    width: '100%',
+    textAlign: 'center',
+  },
   image: {
-    width: 300,
-    height: 100,
-    alignItems: 'center'
+    flex: 1
+  },
+  num: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  idx: {
+    width: 15
+  },
+  name: {
+    flex: 3,
+    textAlign: 'center',
+  },
+  imageTop: {
+    flex: 1,
+    textAlign: 'center'
+  },
+  numTop: {
+    flex: 1,
+    textAlign: 'center'
+  },
+  idxTop: {
+    width: 15,
+    textAlign: 'center'
+  },
+  nameTop: {
+    flex: 3,
+    textAlign: 'center',
   }
 })
 
