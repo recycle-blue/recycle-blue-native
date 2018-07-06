68d8449e35278af63a45218f8fdcba9d2fda0051
AndrewTrahan <globalART19@gmail.com>
2018-07-05 22:07:30 -0500

    updated maps

--- client/components/Map/ad-marker.js
+++ client/components/Map/ad-marker.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card, CardItem, Body, Text, Button, Right, Icon } from 'native-base'
+import { TouchableHighlight, Text, StyleSheet } from 'react-native'
+import { Card, CardItem, Body, Button, Right, Icon } from 'native-base'
 import { Popup } from 'react-native-map-link'
 import { withNavigation } from 'react-navigation'
 import { showDetailAction } from '../../store/location'
 import AdCard from '../Ad/ad-card'
+import { colors } from '../color-palette'
 
 class AdMarker extends React.Component {
   constructor() {
@@ -32,17 +34,19 @@ class AdMarker extends React.Component {
         ad={{ ...marker.ad, distance: marker.distance }}
         navigation={navigation}
       >
-        <CardItem>
-          <Body>
-            <Button success onPress={this.handlePress}>
-              <Text> Navigate to Location </Text>
-            </Button>
-          </Body>
-          <Right>
-            <Button transparent onPress={this.closeDetail}>
-              <Icon name="close" style={{ color: 'black', fontSize: 40 }} />
-            </Button>
-          </Right>
+        <CardItem style={{ justifyContent: 'space-between' }} >
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.handleSubmit}
+            underlayColor={colors.light}
+          >
+            <Text style={styles.buttonFont}>
+              Navigate to Location
+              </Text>
+          </TouchableHighlight>
+          <Button transparent onPress={this.closeDetail}>
+            <Icon name="close" style={{ color: 'black', fontSize: 40 }} />
+          </Button>
         </CardItem>
         <Popup
           isVisible={this.state.isVisible}
@@ -80,5 +84,24 @@ const ConnectedAdMarker = connect(
   mapState,
   mapDispatch
 )(AdMarker)
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 5,
+    paddingVertical: 7,
+    paddingHorizontal: 25,
+    backgroundColor: colors.midLight,
+    borderRadius: 100,
+    borderColor: colors.midLight,
+    borderWidth: 1,
+    alignSelf: 'center',
+  },
+  buttonFont: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+})
 
 export default withNavigation(ConnectedAdMarker)
